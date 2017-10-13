d24019f75e0ca83cc4a15b50f692e88f8e1e8364
github username <github email address>
2017-10-13 13:21:33 -0400

    fix pictures

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -35,21 +35,21 @@ const propsObj = {
 const info = {
 	one: {
 		title: 'The Bottom Line',
-		img: 'https://photos-5.dropbox.com/t/2/AADCGGLlbLlxQG40QuTOoHTS9lOaaggSPNpXCqWCIO6hGQ/12/39354217/png/32x32/3/1507881600/0/2/Screen%20Shot%202017-10-12%20at%209.23.55%20PM.png/EMi-gR4Y14EDIAIoAg/wPYdwr15_Kj1i5qe9wsJtYqG1xGRN2XtnzJlJzMr0Mk?dl=0&size=1280x960&size_mode=3',
+		img: 'https://lh5.googleusercontent.com/NMa-Od3edLCUkvy6s9RclnOAH1vGykOa2_BPq4jEh10ImkAcDkcrj1_sJNENsyXAr4ol3XHMlofrVYY=w1276-h636-rw',
 		desc: 'A simple way for investors to analyze potential cash flow, for rental properties they\'re looking to purchase.',
 		demo: 'https://the-bottom-line.herokuapp.com',
 		github: 'https://github.com/aschittone/the-bottom-line-frontend'
 	},
 	two: {
 		title: 'Breed MatchUp',
-		img: 'https://photos-2.dropbox.com/t/2/AACQ9h_r_sHjJjGBncnsqYcmD1BkR0lMTElz2PRX-s_0WQ/12/39354217/png/32x32/3/1507881600/0/2/Screen%20Shot%202017-10-12%20at%209.25.45%20PM.png/EMi-gR4Y14EDIAIoAg/8xQA0Uo5fuzZVCwXU9Lz4UOdRx2OReKo2ysNWvlmVr4?dl=0&size=1280x960&size_mode=3',
+		img: 'https://lh6.googleusercontent.com/L6B9Wd8fR-TR3aKZnh9qdmad29z3LipFEOipSv8gQ6rJV6sw7xRg_9pr_RDhfGdtmtgsZMprDelR0wE=w1276-h636-rw',
 		desc: 'Enter your medium.com username and match your personality up with a dog breed that you\'ll fit well with.',
 		demo: 'https://breed-matchup.herokuapp.com',
 		github: 'https://github.com/aschittone/breed-matchup-frontend'
 	},
 	three: {
 		title: 'Battleship',
-		img: 'https://photos-2.dropbox.com/t/2/AACeII3hpNTu22opxuAxuopy5ec42HSmyGwm_rCmpRhAGQ/12/39354217/png/32x32/3/1507881600/0/2/Screen%20Shot%202017-10-12%20at%209.25.28%20PM.png/EMi-gR4Y14EDIAIoAg/YbB1D82bQfhb2X5sZ8OYaGbMgmqospYTkAOi-cWThrE?dl=0&size=1280x960&size_mode=3',
+		img: 'https://lh6.googleusercontent.com/CziCxDruAvQ1nvRFTbpofTp0Yh0dYZKk4PL-5pWJYEAmxF_iW48C9FzihdtE0eNWU217ZG0BCZGHEuA=w1276-h636-rw',
 		desc: 'A modern replication of the classic multiplayer game: Battleship. Both players play on the same computer.',
 		demo: 'https://ship-battle.herokuapp.com',
 		github: 'https://github.com/aschittone/battleship-full'
