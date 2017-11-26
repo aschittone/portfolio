991f8557da7233d241774f2610450f521b7ca360
github username <a_schittone@yahoo.com>
2017-11-26 14:45:25 -0500

    update jQuery for scroll

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import './App.css';
 import Portfolio from './components/Portfolio'
 import $ from 'jquery'
 
+function scrollOpacity() {
+  var header = $("header");
+  var range = 200;
+  var scrollTop = $(this).scrollTop(),
+    height = header.outerHeight(),
+    offset = height / 1.25,
+    calc = 1 - (scrollTop - offset + range) / range;
+  header.css({ opacity: calc });
+  if (calc > "1") {
+    header.css({ opacity: 1 });
+  } else if (calc < "0") {
+    header.css({ opacity: 0 });
+  }
+}
+
 class App extends Component {
 
   componentDidMount() {
-    var header = $("header");
-    var range = 200;
-    $(window).on("scroll", function () {
-      var scrollTop = $(this).scrollTop(),
-        height = header.outerHeight(),
-        offset = height / 1.25,
-        calc = 1 - (scrollTop - offset + range) / range;
-
-      header.css({ opacity: calc });
-
-      if (calc > "1") {
-        header.css({ opacity: 1 });
-      } else if (calc < "0") {
-        header.css({ opacity: 0 });
-      }
-    });
+    $(window).on("scroll", scrollOpacity);
   }
 
   render() {
